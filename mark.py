03a896d55a2ed2607b421a9054311f3898944a95
Nimni Senarathna <116495284+NimniSenarathna@users.noreply.github.com>
2023-04-05 21:33:19 +0530

    Create mark.py

---
+++ mark.py
@@ -0,0 +1,16 @@
+mark = int(input("Enter exam mark: "))
+
+if mark >= 90:
+    grade = "A+"
+elif mark >= 80:
+    grade = "A"
+elif mark >= 70:
+    grade = "B"
+elif mark >= 60:
+    grade = "C"
+elif mark >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your grade is:", grade)
